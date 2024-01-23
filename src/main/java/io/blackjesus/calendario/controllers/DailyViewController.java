6592452927f13091fd86260e5672d059155c9ff7
enerbe <szivaknorbert@gmail.com>
2024-01-23 19:17:02 +0100

    Napi nézet
    Megjelenek az események

--- src/main/java/io/blackjesus/calendario/controllers/DailyViewController.java
+++ src/main/java/io/blackjesus/calendario/controllers/DailyViewController.java
@@ -1,12 +1,21 @@
 package io.blackjesus.calendario.controllers;
 
+import io.blackjesus.calendario.managers.EventManager;
+import io.blackjesus.calendario.models.CalendarEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.event.ActionEvent;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 
 public class DailyViewController {
 
@@ -21,6 +30,12 @@ public class DailyViewController {
 
     @FXML
     private DatePicker datePicker;
+
+    private LocalDate date;
+
+    @FXML
+    private VBox eventsContainer;
+
 
     // Az aktuális év és hónap, nap, nap neve
     private String currentYearMonth;
@@ -63,7 +78,9 @@ public class DailyViewController {
         datePicker.setOnAction(e -> handleDatePickerAction());
         if (yearmonth.getText().equals("") && day.getText().equals("") && dayname.getText().equals("")){
             setDate(LocalDate.now());
+            date = LocalDate.now();
         }
+
     }
 
     // Gombnyomásra az aktuális dátumot beállító metódus
@@ -71,6 +88,7 @@ public class DailyViewController {
     void nowButton(ActionEvent event) {
         LocalDate currentDate = LocalDate.now();
         setDate(currentDate);
+        date = currentDate;
     }
 
     // Eseményfigyelő a DatePicker-hez, amikor új dátumot választunk ki
@@ -79,20 +97,60 @@ public class DailyViewController {
 
         if (selectedDate != null) {
             setDate(selectedDate);
+            date = selectedDate;
         }
     }
 
     // Dátum beállító metódus
-    void setDate(LocalDate date) {
-        currentYearMonth = date.getYear() + " " + getShortMonthName(date.getMonthValue());
+    void setDate(LocalDate setdate) {
+        eventsContainer.getChildren().clear();
 
-        currentDay = date.getDayOfMonth();
+        currentYearMonth = setdate.getYear() + " " + getShortMonthName(setdate.getMonthValue());
 
-        currentDayName = date.getDayOfWeek().toString();
+        currentDay = setdate.getDayOfMonth();
+
+        currentDayName = setdate.getDayOfWeek().toString();
+
+
+        //Eventek hozzáadása
+        List<CalendarEvent> events = EventManager.getEventsOnDate(setdate);
+        if(!events.isEmpty()) {
+            int i = 0;
+            while(i < 3 && i < events.size()) {
+                HBox eventContainer = createCalendarEventBox(events.get(i));
+                eventsContainer.getChildren().add(eventContainer);
+                i++;
+            }
+        }
 
         // Értékek beállítása a felületen
         yearmonth.setText(currentYearMonth);
         day.setText(String.valueOf(currentDay));
         dayname.setText(convertToHungarian(currentDayName));
     }
+
+    private HBox createCalendarEventBox(CalendarEvent calendarEvent) {
+        HBox container = new HBox();
+        container.setCursor(Cursor.HAND);
+        String bgColor = "";
+        switch (calendarEvent.getType()) {
+            case EVENT -> bgColor = "rgb(121,134,203)";
+            case TASK -> bgColor = "rgb(66, 133, 244)";
+            case REMINDER -> bgColor = "rgb(142,36,170)";
+            default -> bgColor = "BLACK";
+        }
+        container.setPadding(new Insets(5, 10, 5, 10));
+
+        container.setStyle("-fx-background-color: " + bgColor + "; -fx-background-radius: 15;");
+
+
+        Label titleLabel = new Label();
+        titleLabel.setText(calendarEvent.getTitle());
+        titleLabel.setTextFill(Color.WHITE);
+        titleLabel.setStyle("-fx-strikethrough: true; -fx-font-size: 21px;");
+
+        container.getChildren().add(titleLabel);
+        return container;
+    }
+
 }
